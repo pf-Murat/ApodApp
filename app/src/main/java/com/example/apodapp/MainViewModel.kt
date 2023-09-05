9af80c01840d03d55b6f2c83c1fbaee981c0dce2
Murat <murat.varol@propertyfinder.ae>
2023-09-05 11:33:57 +0300

    Added datetime picker and service connection for dates. (Break for interrupted by snowplow) used minutes 60 mins, total: 137/480 mins

--- app/src/main/java/com/example/apodapp/MainViewModel.kt
+++ app/src/main/java/com/example/apodapp/MainViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.apodapp
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.apodapp.ui.theme.ApodApi
+import com.example.apodapp.ui.theme.ApodItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,14 +16,39 @@ private const val BASE_URL =  "https://api.nasa.gov/planetary/"
 
 class MainViewModel : ViewModel() {
 
-    val apodApi by lazy { getApodApi() }
+    private val apodApi by lazy { getApodApi() }
+    var apods: MutableStateFlow<List<ApodItem>> = MutableStateFlow(emptyList())
+        private set
+    var errorState: MutableStateFlow<String?> = MutableStateFlow(null)
+        private set
 
-    fun generateImageList(): List<String> {
-        val list = mutableListOf<String>()
-        repeat(10){ counter ->
-            list.add("Sample $counter")
+    fun clearErrorState() {
+        errorState.update { null }
+    }
+
+    fun getApods(
+        startDate: String? = null,
+        endDate: String? = null,
+        count: Int? = null
+    ) {
+        viewModelScope.launch {
+            try {
+                val response = apodApi.getApods(
+                    startDate = startDate,
+                    endDate = endDate,
+                    count = count
+                )
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        apods.update { apodList -> apodList + response.body().orEmpty() }
+                    } else {
+                        errorState.update { "Empty List Fetched" }
+                    }
+                }
+            } catch (ex: Throwable) {
+                errorState.update { ex.cause?.message ?: "ERROR WITH EMPTY MESSAGE" }
+            }
         }
-        return list.toList()
     }
 }
 
--- app/src/main/java/com/example/apodapp/SearchScreen.kt
+++ app/src/main/java/com/example/apodapp/SearchScreen.kt
@@ -1,31 +1,93 @@
 package com.example.apodapp
 
+import android.app.DatePickerDialog
+import android.util.Log
+import android.widget.DatePicker
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import java.util.Calendar
+
+
+private const val PAGE_SIZE = 20
 
 @Composable
 fun SearchScreen(viewModel: MainViewModel) {
-    Box(
+    val context = LocalContext.current
+    val apodList by viewModel.apods.collectAsStateWithLifecycle()
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val calendarStart = Calendar.getInstance()
+        val calendarEnd = Calendar.getInstance()
+        calendarStart.add(Calendar.DATE, -PAGE_SIZE)
+
+        var startDateForBackend by remember { mutableStateOf(calendarStart.getBackEndTime()) }
+        var endDateForBackend by remember { mutableStateOf(calendarEnd.getBackEndTime()) }
+
+        val datePickerStart = DatePickerDialog(
+            context,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+                startDateForBackend = "$selectedYear-$selectedMonth-$selectedDayOfMonth"
+                viewModel.getApods(
+                    startDate = startDateForBackend,
+                    endDate = endDateForBackend
+                )
+            },
+            calendarStart[Calendar.YEAR],
+            calendarStart[Calendar.MONTH],
+            calendarStart[Calendar.DAY_OF_MONTH]
+        )
+
+        val datePickerEnd = DatePickerDialog(
+            context,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+                endDateForBackend = "$selectedYear-$selectedMonth-$selectedDayOfMonth"
+            },
+            calendarEnd[Calendar.YEAR],
+            calendarEnd[Calendar.MONTH],
+            calendarEnd[Calendar.DAY_OF_MONTH]
+        )
+
+        Row {
+            Button(
+                onClick = { datePickerStart.show() }
+            ) {
+                Text(text = startDateForBackend)
+            }
+            Button(
+                onClick = { datePickerEnd.show() }
+            ) {
+                Text(text = endDateForBackend)
+            }
+        }
+
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(viewModel.generateImageList()) { item ->
+            items(apodList) { item ->
                 Card(
                     modifier = Modifier
                         .fillMaxSize(.9f)
@@ -36,12 +98,24 @@ fun SearchScreen(viewModel: MainViewModel) {
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
-                            text = item,
+                            text = item.date,
                             textAlign = TextAlign.Center
                         )
                     }
                 }
             }
         }
+
+        val date = Calendar.getInstance()   // 19-01-2018
+        date.add(Calendar.DATE, +7)
+        Log.d(
+            "DateTime: ",
+            "${date.get(Calendar.YEAR)}-${
+                date.get(Calendar.MONTH).inc()
+            }-${date.get(Calendar.DAY_OF_MONTH)}"
+        )         // 12-01-2018
     }
 }
+
+private fun Calendar.getBackEndTime() =
+    "${this[Calendar.YEAR]}-${this[Calendar.MONTH].inc()}-${this[Calendar.DAY_OF_MONTH]}"
